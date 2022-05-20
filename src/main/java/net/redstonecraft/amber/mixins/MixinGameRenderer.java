502193a11fccb9fb188a096059f023912dc1e867
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2022-05-20 19:02:14 +0200

    temporarily fix crash

--- src/main/java/net/redstonecraft/amber/mixins/MixinGameRenderer.java
+++ src/main/java/net/redstonecraft/amber/mixins/MixinGameRenderer.java
@@ -18,19 +18,19 @@ public class MixinGameRenderer {
 
     @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;", ordinal = 1))
     private void injectRender(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
-        if (MinecraftClient.getInstance().currentScreen == null) {
-            Framebuffer clientFb = MinecraftClient.getInstance().getFramebuffer();
-            Vector2f size = new Vector2f(Amber.getBlurFb1().getWidth(), Amber.getBlurFb1().getHeight());
-            Amber.getBlurFb1().bind();
-            Amber.getHoriBlurRenderer().setTexture(new Texture(clientFb.getColorAttachment(), clientFb.textureWidth, clientFb.textureHeight, GL_RGBA));
-            Amber.getHoriBlurRenderer().render(new Vector2f(0, 0), size, new Vector2f(0, 0), new Vector2f(1, 1));
-            Amber.getHoriBlurRenderer().finish();
-            Amber.getBlurFb2().bind();
-            Amber.getVertBlurRenderer().setTexture(Amber.getHoriBlurRenderer().getTexture());
-            Amber.getVertBlurRenderer().render(new Vector2f(0, 0), size, new Vector2f(0, 0), new Vector2f(1, 1));
-            Amber.getVertBlurRenderer().finish();
-            clientFb.beginWrite(false);
-        }
+//        if (MinecraftClient.getInstance().currentScreen == null) {
+//            Framebuffer clientFb = MinecraftClient.getInstance().getFramebuffer();
+//            Vector2f size = new Vector2f(Amber.getBlurFb1().getWidth(), Amber.getBlurFb1().getHeight());
+//            Amber.getBlurFb1().bind();
+//            Amber.getHoriBlurRenderer().setTexture(new Texture(clientFb.getColorAttachment(), clientFb.textureWidth, clientFb.textureHeight, GL_RGBA));
+//            Amber.getHoriBlurRenderer().render(new Vector2f(0, 0), size, new Vector2f(0, 0), new Vector2f(1, 1));
+//            Amber.getHoriBlurRenderer().finish();
+//            Amber.getBlurFb2().bind();
+//            Amber.getVertBlurRenderer().setTexture(Amber.getHoriBlurRenderer().getTexture());
+//            Amber.getVertBlurRenderer().render(new Vector2f(0, 0), size, new Vector2f(0, 0), new Vector2f(1, 1));
+//            Amber.getVertBlurRenderer().finish();
+//            clientFb.beginWrite(false);
+//        }
     }
 
 }
