d976b5098be404f69348906925aee390c95f8638
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2023-03-13 23:25:00 +0100

    continue merge quilt init

--- src/main/kotlin/net/redstonecraft/amber/modules/modules/Categories.kt
+++
@@ -1,7 +0,0 @@
-package net.redstonecraft.amber.modules.modules
-
-import net.redstonecraft.amber.modules.Category
-
-object MiscCategory: Category("Misc", "Everything that does not fit somewhere else.")
-object WorldCategory: Category("World", "Everything in the Minecraft world.")
-object RenderCategory: Category("Render", "All stuff about rendering.")
--- src/main/kotlin/net/redstonecraft/amber/modules/modules/misc/NarratorDisablerModule.kt
+++
@@ -1,6 +0,0 @@
-package net.redstonecraft.amber.modules.modules.misc
-
-import net.redstonecraft.amber.modules.ToggleModule
-import net.redstonecraft.amber.modules.modules.MiscCategory
-
-object NarratorDisablerModule: ToggleModule("NarratorDisabler", "Forces the narrator to do nothing if on.", MiscCategory)
